d11bb645f2caf4a5780ade1a56e9f294b45ad056
Laura Wu <laurawu@email.unc.edu>
2022-04-11 19:31:24 -0400

    Created a react app

---
+++ covid-19-tracker/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <H1>Let's build a Covid-19 Tracking Program</H1>
+    </div>
+  );
+}
+
+export default App;
