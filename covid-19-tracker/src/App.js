4beed18354da392dc0e462539d49b3e7ee52776d
Laura Wu <laurawu@email.unc.edu>
2022-04-11 22:19:39 -0400

    Created the drop down menu

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState([]); 
+
+  useEffect(() => {
+    const getStateData = async() => {
+      await fetch('https://disease.sh/v3/covid-19/states')
+      .then((response) => response.json())
+      .then((data) => {
+        const states = data.map((state) => (
+          {
+            name: state.state,
+          }
+        ));
+        setState(states)
+      });
+    };
+    getStateData();
+  }, [])
+
   return (
     <div className="app">
-      <h1>COVID-19 TRACKER</h1>
+        <div className="header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className='dropdown'>
+            <Select variant= 'outlined' value= ''>
+            <MenuItem name="USA">USA</MenuItem>
+              {state.map((state) =>
+              (<MenuItem name={state.name}>{state.name}</MenuItem>))}
+              </Select> 
 
+          </FormControl>
       {/* Header */}
       {/* Title + select input drop down field */}
       {/* Infobox */}
@@ -16,6 +43,8 @@ function App() {
       {/* Graphs */}
 
       {/* Map */}
+
+      </div>
 
     </div>
   );
