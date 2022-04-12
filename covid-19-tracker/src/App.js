e79ee659a93f3f05b191810bb9a393618fdd1016
Shawn Khandia <shawnk04@email.unc.edu>
2022-04-12 01:14:19 -0400

    Minor formatting changes

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,9 +1,10 @@
 import { FormControl, MenuItem, Select } from '@material-ui/core';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
   const [state, setState] = useState([]); 
+  document.title = "Okab COVID-19 Dashboard"
 
   useEffect(() => {
     const getStateData = async() => {
@@ -23,18 +24,31 @@ const App = () => {
 
   return (
     <div className="app">
-        <div className="header">
-          <h1>COVID-19 TRACKER</h1>
-          <FormControl className='dropdown'>
-            <Select variant= 'outlined' value= ''>
-            <MenuItem name="USA">USA</MenuItem>
-              {state.map((state) =>
-              (<MenuItem name={state.name}>{state.name}</MenuItem>))}
-              </Select> 
-
-          </FormControl>
       {/* Header */}
+        <div className="header">
+          <h1>Okab COVID-19 Dashboard</h1>
+
+      {/* User Sign Up and Log In Buttons */}
+      <section id="user-sign-log-container">
+        {/* Sign Up Button */}
+        <button id="startSignUp">Sign Up</button>
+
+        {/* Log In Button */}
+        <button id="startLogIn">Log In</button>
+      </section>
+
+      <section id="firebaseui-auth-container"></section>
+
       {/* Title + select input drop down field */}
+        <FormControl className='dropdown'>
+          <Select variant= 'outlined' value= ''>
+          <MenuItem name="USA">USA</MenuItem>
+            {state.map((state) =>
+            (<MenuItem name={state.name}>{state.name}</MenuItem>))}
+            </Select> 
+
+        </FormControl>
+      
       {/* Infobox */}
       {/* Infobox */}
       {/* Infobox */}
