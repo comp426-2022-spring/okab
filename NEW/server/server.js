92f707c123a2f9b42edcd43430fcf8c15dc92a49
Laura Wu <laurawu@email.unc.edu>
2022-04-12 16:56:39 -0400

    Created NEW that has a working API and React

---
+++ NEW/server/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const minimist = require('minimist')
+
+// Require Express.js
+const app = express()
+const args = minimist(process.argv.slice(2))
+args["port"];
+var HTTP_PORT = args.port || 5000;
+
+
+// Start an app server
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
+
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;       
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+    });
+
+app.post('/login', (req, res) => {
+    
+})    
