e0c89869e2edbf732ca0c557d362628b6c0029d9
Shawn Khandia <shawnk04@email.unc.edu>
2022-04-12 20:09:47 -0400

    Implemented User "Sign Up," "Log In," "Log Out"

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -2,9 +2,29 @@ import { FormControl, MenuItem, Select } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { auth } from "./firebase";
+
 const App = () => {
   const [state, setState] = useState([]); 
-  document.title = "Okab COVID-19 Dashboard"
+  document.title = "Okab COVID-19 Dashboard";
+  document.body.style = 'background: rgb(0, 47, 95);';
+
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
 
   useEffect(() => {
     const getStateData = async() => {
@@ -22,43 +42,129 @@ const App = () => {
     getStateData();
   }, [])
 
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+  
   return (
     <div className="app">
       {/* Header */}
-        <div className="header">
-          <h1>Okab COVID-19 Dashboard</h1>
+      <div className="header">
+        <h1>Okab COVID-19 Dashboard</h1>
+      </div>
+
 
       {/* User Sign Up and Log In Buttons */}
-      <section id="user-sign-log-container">
-        {/* Sign Up Button */}
-        <button id="startSignUp">Sign Up</button>
+      <div className="buttons">
+        <section id="user-sign-log-container">
+          {/* Sign Up */}
+          <div>
+            <h3> Sign Up </h3>
+            <input
+              placeholder="Email..."
+              onChange={(event) => {
+                setRegisterEmail(event.target.value);
+              }}
+            />
+            <input
+              placeholder="Password..."
+              type="password"
+              name="password"
+              onChange={(event) => {
+                setRegisterPassword(event.target.value);
+              }}
+            />
 
-        {/* Log In Button */}
-        <button id="startLogIn">Log In</button>
-      </section>
+            <button 
+            onClick={register}
+            > Create User</button>
+          </div>
+
+          {/* Log In */}
+          <div>
+            <h3> Login </h3>
+            <input
+              placeholder="Email..."
+              onChange={(event) => {
+                setLoginEmail(event.target.value);
+              }}
+            />
+            <input
+              placeholder="Password..."
+              type="password"
+              name="password"
+              onChange={(event) => {
+                setLoginPassword(event.target.value);
+              }}
+            />
+
+            <button 
+            onClick={login}
+            > Login</button>
+          </div>
+
+          {/* Sign Out */}
+          <h4> User Signed In As: </h4>
+          {user?.email}
+
+          <button 
+          onClick={logout}
+          > Sign Out </button>
+
+        </section>
+      </div>
 
       <section id="firebaseui-auth-container"></section>
 
+
       {/* Title + select input drop down field */}
+      <div className="dropdown">
         <FormControl className='dropdown'>
           <Select variant= 'outlined' value= ''>
           <MenuItem name="USA">USA</MenuItem>
             {state.map((state) =>
             (<MenuItem name={state.name}>{state.name}</MenuItem>))}
             </Select> 
-
         </FormControl>
+      </div>
       
+
       {/* Infobox */}
       {/* Infobox */}
       {/* Infobox */}
+
 
       {/* Tables */}
       {/* Graphs */}
 
-      {/* Map */}
 
-      </div>
+      {/* Map */}
 
     </div>
   );
