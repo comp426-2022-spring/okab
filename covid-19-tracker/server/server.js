9d9ba56ab1a9a5fe4368eb0b207f96a496a0b6f1
Laura Wu <laurawu@email.unc.edu>
2022-04-20 19:56:45 -0400

    Added log

--- covid-19-tracker/server/server.js
+++ covid-19-tracker/server/server.js
@@ -1,12 +1,61 @@
-const express = require('express')
-const minimist = require('minimist')
+// Place your server entry point code here
+// Require minimist module
+const args = require('minimist')(process.argv.slice(2))
+// See what is stored in the object produced by minimist
+console.log(args)
+// Store help text 
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
 
 // Require Express.js
+var express = require("express")
 const app = express()
-const args = minimist(process.argv.slice(2))
+const db = require('./database.js')
+const morgan = require('morgan')
+const fs = require('fs')
+
+// Serve static HTML files
+app.use(express.static('./public'));
+// Allow JSON body messages on all endpoints
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 args["port"];
 var HTTP_PORT = args.port || 5000;
 
+if(args.log == 'false') {
+    console.log("Not creating a new access.log")
+}
+else {
+    // Use morgan for logging to files
+    // Create a write stream to append (flags: 'a') to a file
+
+    //./data/log/access.log
+    const WRITESTREAM = fs.createWriteStream('access.log', { flags: 'a' })
+    // Set up the access logging middleware
+    app.use(morgan('combined', { stream: WRITESTREAM }))
+}
 
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
@@ -22,6 +71,7 @@ app.get('/app/', (req, res) => {
         res.end(res.statusCode+ ' ' +res.statusMessage)
     });
 
-app.post('/login', (req, res) => {
-    
-})    
+// default response for any other request
+app.use(function(req, res){
+    res.status(404).end('404 NOT FOUND')
+})
