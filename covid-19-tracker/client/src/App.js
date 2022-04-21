d8d56e7fd30f9f4fd39df2dc47f70763ac09052f
Shawn Khandia <shawnk04@email.unc.edu>
2022-04-20 21:23:23 -0400

    Updated error messgaes on login/signup errors

--- covid-19-tracker/client/src/App.js
+++ covid-19-tracker/client/src/App.js
@@ -43,50 +43,50 @@ const App = () => {
   }, [])
 
   const register = async () => {
+    var signEMAIL = document.getElementById("signEMAIL");
+    var signPW = document.getElementById("signPW");
+    var signError = document.getElementById("signError");
     try {
       const user = await createUserWithEmailAndPassword(
         auth,
         registerEmail,
         registerPassword
       );
-      var signEMAIL = document.getElementById("signEMAIL");
-      var signPW = document.getElementById("signPW");
       console.log("User registered");
       console.log(user);
       signEMAIL.value = "";
       signPW.value = "";
+      signError.style.visibility = "hidden";
     } 
-    
     catch (error) {
-      var signEMAIL = document.getElementById("signEMAIL");
-      var signPW = document.getElementById("signPW");
       console.log(error.message);
-      signEMAIL.value = error.message;
+      signEMAIL.value = "";
       signPW.value = "";
+      signError.style.visibility = "visible";
     }
   };
 
   const login = async () => {
+    var logEMAIL = document.getElementById("logEMAIL");
+    var logPW = document.getElementById("logPW");
+    var logError = document.getElementById("logError");
     try {
       const user = await signInWithEmailAndPassword(
         auth,
         loginEmail,
         loginPassword
       );
-      var logEMAIL = document.getElementById("logEMAIL");
-      var logPW = document.getElementById("logPW");
       console.log("User logged in");
       console.log(user);
       logEMAIL.value = "";
       logPW.value = "";
+      logError.style.visibility = "hidden";
     } 
-    
     catch (error) {
-      var logEMAIL = document.getElementById("logEMAIL");
-      var logPW = document.getElementById("logPW");
       console.log(error.message);
-      logEMAIL.value = error.message;
+      logEMAIL.value = "";
       logPW.value = "";
+      logError.style.visibility = "visible";
     }
   };
 
@@ -106,20 +106,23 @@ const App = () => {
       {/* User Sign Up and Log In Buttons */}
       {/* 
       
-          TODO: - Display error cases
+          Error cases
                 1. Incorrect email or password when logging in
                 2. Email already in use when signing up
                 3. Invalid email (not in email format) when signing up
                 4. Password not at least 6 letters long when signing up
                 5. User logging out when not logged in
 
-      
+          TODO: Add ability to change user info and delete account
+
       */}
       <div className="buttons">
         <section id="user-sign-log-container">
           {/* Sign Up */}
           <div>
             <h3> Sign Up </h3>
+            <label id="signError" class="signError">Error: Invalid Email and/or Password</label>
+            <br/>
             <input
               placeholder="Email"
               id="signEMAIL"
@@ -144,7 +147,10 @@ const App = () => {
 
           {/* Log In */}
           <div>
+            <br/>
             <h3> Login </h3>
+            <label id="logError" class="logError">Error: Invalid Email and/or Password</label>
+            <br/>
             <input
               placeholder="Email"
               id="logEMAIL"
@@ -168,6 +174,7 @@ const App = () => {
           </div>
 
           {/* Sign Out */}
+          <br/>
           <h4> User Signed In As: </h4>
           {user?.email}
 
