5228e7a0fce89c7b96e045faa98518d0787ca9cd
Shawn Khandia <shawnk04@email.unc.edu>
2022-05-01 16:03:47 -0400

    Updated index to do auth

--- covid-19-tracker/server/index.js
+++ covid-19-tracker/server/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import cors from 'cors'
 import { User } from "./config.js";
-import { getFirestore, collection, addDoc, updateDoc, serverTimestamp, deleteDoc, doc } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { auth } from "./firebase";
+import { getFirestore, collection, addDoc, updateDoc, serverTimestamp } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 // Firebase config
@@ -15,14 +22,17 @@ const firebaseConfig = {
     measurementId: "G-8978S7Z8XF"
   };
 
-// Initialize firestore db
+// Initialize firestore db, auth
 const fbapp = initializeApp(firebaseConfig);
 const db = getFirestore(fbapp);
+const auth = getAuth(app);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+/* Endoints */
 app.get('/app/', (req, res) => {
     res.json({"message":"Your API works! (200)"})
     res.status(200)
@@ -37,17 +47,29 @@ app.get("/", async (req, res) => {
 app.post("/create", async (req, res) => {
   const data = req.body;
   try {
-    const docRef = await addDoc(collection(db, "Users"), {
-        name: data.name,
-        password: data.password
-    });
-    console.log("Document written with ID: ", docRef.id);
-    const updateTimestamp = await updateDoc(docRef, {
-        time_created: serverTimestamp()
-    });
-} catch (e) {
-    console.error("Error adding document: ", e);
-}
+    const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+    );
+    try {
+        const docRef = await addDoc(collection(db, "Users"), {
+            name: registerEmail,
+            password: registerPassword
+        });
+        console.log("Document written with ID: ", docRef.id);
+        const updateTimestamp = await updateDoc(docRef, {
+            time_created: serverTimestamp()
+        });
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+      console.log("User registered");
+      console.log(user);
+  }
+  catch (error) {
+      console.log(error.message);
+  }
   console.log("Data of Users", data);
   res.send({msg: "User Added"});
 });
